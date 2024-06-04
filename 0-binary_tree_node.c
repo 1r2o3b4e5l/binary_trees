45830e6b6e86816ae85eeed496e65c2ee2c9d219
1r2o3b4e5l <robelamsalu44@gmail.com>
2024-06-04 14:09:45 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
-
-void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -17,43 +14,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     new_node->right = NULL;
 
     return new_node;
-}
-
-binary_tree_t *create_binary_tree()
-{
-    binary_tree_t *root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
-
-    return root;
-}
-
-int main(void)
-{
-    binary_tree_t *root = create_binary_tree();
-
-    if (root == NULL)
-    {
-        fprintf(stderr, "Failed to create binary tree\n");
-        return EXIT_FAILURE;
-    }
-
-    binary_tree_print(root);
-
-    return EXIT_SUCCESS;
-}
-
-void binary_tree_print(const binary_tree_t *tree)
-{
-    if (tree == NULL)
-        return;
-
-    printf("%d\n", tree->n);
-    binary_tree_print(tree->left);
-    binary_tree_print(tree->right);
 }
 
